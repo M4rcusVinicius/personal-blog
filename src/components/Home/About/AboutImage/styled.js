71771fee4758fca0d5546315768206be42968eed
M4rcusVinicius <marcus.vinicius.mvap@gmail.com>
2021-01-02 15:04:23 -0300

    About Image

--- src/components/Home/About/AboutImage/styled.js
+++ src/components/Home/About/AboutImage/styled.js
@@ -2,8 +2,8 @@ import styled from 'styled-components'
 import Img from 'gatsby-image'
 
 export const AboutImageWrapper = styled(Img)`
-  height: 25rem;
-  width: 20rem;
+  height: 68%;
+  width: 28%;
   margin-left: 3rem;
   border-radius: 15px;
 
