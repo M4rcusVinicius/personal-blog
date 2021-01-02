243c2856589785d8700cfee37ae993181b559df5
M4rcusVinicius <marcus.vinicius.mvap@gmail.com>
2021-01-02 09:42:16 -0300

    Logo Image

--- src/components/Logo/LogoImage/index.js
+++ src/components/Logo/LogoImage/index.js
@@ -6,7 +6,7 @@ import * as S from './styled'
 const LogoImage = () => {
   const { logoImage } = useStaticQuery(graphql`
     query {
-      logoImage: file(relativePath: { eq: "logo-dark.png" }) {
+      logoImage: file(relativePath: { eq: "logo.png" }) {
         childImageSharp {
           fluid(maxWidth: 300) {
             ...GatsbyImageSharpFluid
--- src/components/Logo/LogoImage/styled.js
+++ src/components/Logo/LogoImage/styled.js
@@ -2,7 +2,6 @@ import styled from 'styled-components'
 import Img from 'gatsby-image'
 
 export const LogoImageWrapper = styled(Img)`
-  height: 2.75rem;
-  width: 2.75rem;
-  margin-bottom: 0.2rem;
+  height: 4rem;
+  width: 4rem;
 `
