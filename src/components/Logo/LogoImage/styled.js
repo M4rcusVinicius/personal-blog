6215ccd1fb69bc2d8980112055b370b5e475e8be
M4rcusVinicius <marcus.vinicius.mvap@gmail.com>
2021-01-02 15:05:06 -0300

    Logo Image

--- src/components/Logo/LogoImage/styled.js
+++ src/components/Logo/LogoImage/styled.js
@@ -2,6 +2,6 @@ import styled from 'styled-components'
 import Img from 'gatsby-image'
 
 export const LogoImageWrapper = styled(Img)`
-  height: 4rem;
-  width: 4rem;
+  height: 4.7rem;
+  width: 4.7rem;
 `
