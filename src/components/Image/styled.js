14c38431e285d4e8d5f4417a14cc939653ebf4fe
M4rcusVinicius <marcus.vinicius.mvap@gmail.com>
2021-01-02 21:44:19 -0300

    Image styled

--- src/components/Image/styled.js
+++ src/components/Image/styled.js
@@ -2,8 +2,8 @@ import styled from "styled-components"
 import Img from "gatsby-image"
 
 export const Image = styled(Img)`
-  border-radius: ${props => props.borderRadius ? props.borderRadius : "10px"};
-  height: ${props => props.height ? props.height : "14rem"};
+  border-radius: ${props => props.borderRadius ? props.borderRadius : "9px "};
+  height: ${props => props.height ? props.height : "18rem"};
   width: ${props => props.width};
   max-height: ${props => props.maxHeight};
   max-width: ${props => props.maxWidth};
