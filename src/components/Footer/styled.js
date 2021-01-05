7a1e11b59d9d914976a821c39617553ccd13a248
M4rcusVinicius <marcus.vinicius.mvap@gmail.com>
2021-01-05 01:31:39 -0300

    Footer

--- src/components/Footer/styled.js
+++ src/components/Footer/styled.js
@@ -4,7 +4,7 @@ import { Link } from "gatsby"
 export const FooterWrapper = styled.div`
   width: 100%;
   display: flex;
-  background-color: var(--base);
+  background-color: var(--back);
   justify-content: space-around;
   padding: 2rem;
 `
@@ -20,29 +20,29 @@ export const FooterLink = styled(Link)`
   text-decoration: none;
   font-weight: 600;
   font-size: 1.2rem;
-  color: var(--clear);
+  color: var(--link);
   transition: 200ms;
 
   &:hover {
-    color: var(--blue);
+    color: var(--linkHover);
   }
 `
 
 export const FooterText = styled.p`
   font-weight: 600;
   font-size: 0.8rem;
-  color: var(--clear);
+  color: var(--textSoft);
 `
 
 export const FooterTitle = styled.h3`
   font-weight: 800;
   font-size: 1.4rem;
-  color: var(--soft);
+  color: var(--title);
   transition: color 400ms;
   margin: 0.7em 0;
 
   &:hover {
-    color: var(--blue);
+    color: var(--title);
   }
 `
 
