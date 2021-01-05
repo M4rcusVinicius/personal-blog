aec6d52d7a8cb8fb45120af5f5fa477a0e0afb0d
M4rcusVinicius <marcus.vinicius.mvap@gmail.com>
2021-01-05 01:33:38 -0300

    Add search

--- src/pages/pesquisa.js
+++ src/pages/pesquisa.js
@@ -2,11 +2,12 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-//import Search from "../components/Search"
+import Search from "../components/Search"
 
 const PesquisaPage = () => (
   <Layout>
     <SEO title="Pesquisa" />
+    <Search />
   </Layout>
 )
 
