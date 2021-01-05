fb61e46ebb0db6899f82b0c5dd1407741cc5286d
M4rcusVinicius <marcus.vinicius.mvap@gmail.com>
2021-01-05 01:33:30 -0300

    Styled searck

--- src/components/Search/styled.js
+++ src/components/Search/styled.js
@@ -12,23 +12,36 @@ export const SearchWrapper = styled.section`
   }
   .ais-SearchBox,
   .ais-Stats {
-    padding: 0.5rem 3rem;
+    padding: 0.5rem 6rem;
   }
   .ais-SearchBox {
-    padding-top: 6rem;
+    padding-top: 4.75rem;
   }
   .ais-Stats {
   }
   .ais-SearchBox-input {
     display: flex;
     font-size: 1.6rem;
-    padding: 0.5rem;
-    width: 90%;
+    padding: 0.8rem;
+    width: 100%;
+    color: var(--SearchInput);
+    border-radius: 5px;
+    background-color: var(--back);
     &::placeholder {
     }
   }
   .ais-SearchBox-submit,
   .ais-SearchBox-reset {
     display: none;
+  }
+  .ais-Hits-list {
+    margin: 3rem 6rem;
+
+    body#grid & {
+    display: grid;
+    grid-area: ais-Hits-list;
+    grid-gap: 2rem;
+    grid-template-columns: repeat(auto-fit, minmax(320px, 1fr));
+  }
   }
 `
